bc9613025165af36326e8a832758a1d751126162
AworThales <thals4jesus@gmail.com>
2023-09-12 02:24:52 -0700

    update

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,19 +11,19 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, lower, higher;
-	double k;
+	size_t pos, low, high;
+	double f;
 
 	if (array == NULL)
 		return (-1);
 
-	lower = 0;
-	higher = size - 1;
+	low = 0;
+	high = size - 1;
 
 	while (size)
 	{
-		k = (double)(higher - lower) / (array[higher] - array[lower]) * (value - array[lower]);
-		pos = (size_t)(lower + k);
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
 		printf("Value checked array[%d]", (int)pos);
 
 		if (pos >= size)
@@ -40,11 +40,11 @@ int interpolation_search(int *array, size_t size, int value)
 			return ((int)pos);
 
 		if (array[pos] < value)
-			lower = pos + 1;
+			low = pos + 1;
 		else
-			higher = pos - 1;
+			high = pos - 1;
 
-		if (lower == higher)
+		if (low == high)
 			break;
 	}
 
